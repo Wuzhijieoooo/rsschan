55645929ba2f22df6dfe020073088eedc25df98e
Wuzhijieoooo <31843712+Wuzhijieoooo@users.noreply.github.com>
2022-02-28 17:22:47 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -273,7 +273,7 @@ def pushplus_bot(title: str, content: str) -> None:
     url = "http://www.pushplus.plus/send"
     data = {
         "token": push_config.get("PUSH_PLUS_TOKEN"),
-        "title": title[:100],
+        "title": title.replace("\n", "\\n")[:100],
         "content": content,
         "topic": push_config.get("PUSH_PLUS_USER"),
     }
